27d4627085faf5d0e62901bdefbc9e0eeb9690ea
Steven Thuriot <StevenThuriot@users.noreply.github.com>
2009-11-26 19:54:13 +0000

    Implementing basics of the state pattern

---
+++ Java/src/model/UnfinishedTaskState.java
@@ -0,0 +1,38 @@
+package model;
+
+import exception.DependencyException;
+
+public class UnfinishedTaskState implements TaskState {
+	public Boolean canBeExecuted() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	public boolean canBeFinished() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
+	public Status getStatus() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void setStatus(Status newStatus) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void updateTaskStatus(Status newStatus) throws DependencyException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	public void updateTaskStatusRecursively(Status newStatus) {
+		// TODO Auto-generated method stub
+		
+	}
+}
