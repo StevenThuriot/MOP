3714788ec4b2bb6b4962b86d4531f1b197e70faa
steventhuriot <steventhuriot@8ba516a0-d77e-11de-89a3-1386611daa76>
2009-11-26 20:32:33 +0000

    Extra methods that belong in the state

--- Java/src/model/TaskState.java
+++ Java/src/model/TaskState.java
@@ -1,8 +1,12 @@
 package model;
 
+import java.util.GregorianCalendar;
+
 import exception.*;
 
 public interface TaskState {
+	public void setDescription(String newDescription) throws EmptyStringException, NullPointerException;
+	public void updateTaskTiming(GregorianCalendar newStart, GregorianCalendar newDue, int newDuration) throws BusinessRule1Exception;
 	public boolean canBeFinished();
 	public void updateTaskStatus(Status newStatus) throws DependencyException;
 	public void updateTaskStatusRecursively(Status newStatus);
