4203b03d4f4af06d76e1d5aa930bcdfe26477e42
Steven Thuriot <StevenThuriot@users.noreply.github.com>
2009-11-26 20:32:33 +0000

    Extra methods that belong in the state

--- Java/src/model/UnfinishedTaskState.java
+++ Java/src/model/UnfinishedTaskState.java
@@ -1,37 +1,60 @@
 package model;
 
+import java.util.GregorianCalendar;
+
+import exception.BusinessRule1Exception;
 import exception.DependencyException;
+import exception.EmptyStringException;
 
 public class UnfinishedTaskState implements TaskState {
+
+	@Override
 	public Boolean canBeExecuted() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-
+	@Override
 	public boolean canBeFinished() {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
-
+	@Override
 	public Status getStatus() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	@Override
+	public void setDescription(String newDescription)
+			throws EmptyStringException, NullPointerException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
 	public void setStatus(Status newStatus) {
 		// TODO Auto-generated method stub
 		
 	}
 
+	@Override
 	public void updateTaskStatus(Status newStatus) throws DependencyException {
 		// TODO Auto-generated method stub
 		
 	}
 
-	
+	@Override
 	public void updateTaskStatusRecursively(Status newStatus) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateTaskTiming(GregorianCalendar newStart,
+			GregorianCalendar newDue, int newDuration)
+			throws BusinessRule1Exception {
 		// TODO Auto-generated method stub
 		
 	}
