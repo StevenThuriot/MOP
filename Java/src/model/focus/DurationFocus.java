afb03686f5462582a1e144ceb84366e2e8de155b
vangeneugden <vangeneugden@8ba516a0-d77e-11de-89a3-1386611daa76>
2009-12-04 14:35:50 +0000

    Large update to the Focus.

--- Java/src/model/focus/DurationFocus.java
+++ Java/src/model/focus/DurationFocus.java
@@ -15,14 +15,23 @@ public class DurationFocus extends FocusStrategy {
 		this.minimum = minimum;
 		this.maximum = maximum;
 	}
-	@Override
-	public List<Task> showtasks() {
-		List<Task> tasks = this.filterByDuration(this.getCurrentUser().getTasks());
+	
+	/**
+	 * Sort the tasks according to Duration
+	 * @param tasks The original list of tasks
+	 * @return list of tasks, sorted by Duration
+	 */
+	protected List<Task> sort(List<Task> tasks)
+	{
 		Collections.sort(tasks,new TaskDurationComparator());
 		return tasks;
 	}
-	
-	public List<Task> filterByDuration(List<Task> taken)
+	/**
+	 * Filter tasks according to duration. Only returns tasks where minimumDuration < Task duration < maximum duration
+	 * @param taken The original list of tasks
+	 * @return List of tasks, filtered by minimum and maximum duration
+	 */
+	protected List<Task> filter(List<Task> taken)
 	{
 		ArrayList<Task> filtered = new ArrayList<Task>();
 		for(Task current : taken)
--- Java/src/model/focus/FocusStrategy.java
+++ Java/src/model/focus/FocusStrategy.java
@@ -8,13 +8,50 @@ import model.User;
  * @author bart
  *
  */
-public abstract class FocusStrategy {
+public class FocusStrategy {
 	private User currentUser;
-	public abstract List<Task> showtasks();
-	public User getCurrentUser() {
-		return currentUser;
+	/**
+	 * Method that defines the default protocol for getting a Focus:
+	 * 1. Get tasks
+	 * 2. Filter by certain parameters
+	 * 3. Sort the tasks
+	 * 4. Return tasks
+	 * @return
+	 */
+	public final List<Task> showtasks()
+	{
+		List<Task> tasks = this.getTasks();
+		this.sort(tasks);
+		this.filter(tasks);
+		return tasks;
 	}
-	public void setCurrentUser(User currentUser) {
-		this.currentUser = currentUser;
+	/**
+	 * Get all the tasks from the current user.
+	 * This method can be overrided
+	 * @return
+	 */
+	protected List<Task> getTasks()
+	{
+		return currentUser.getTasks();
+	}
+	/**
+	 * Method to filter tasks according to a certain strategy.
+	 * This method is to be overrided by classes extending FocusStrategy
+	 * @param tasks
+	 * @return
+	 */
+	protected List<Task> filter(List<Task> tasks)
+	{
+		return tasks;
+	}
+	/**
+	 * Method to sort tasks according to a certain strategy.
+	 * This method is to be overrided by classes extending FocusStrategy
+	 * @param tasks
+	 * @return
+	 */
+	protected List<Task> sort(List<Task> tasks)
+	{
+		return tasks;
 	}
 }
