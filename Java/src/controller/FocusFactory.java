f4a21f80273d686333db48007de048b7d37d37a5
vangeneugden <vangeneugden@8ba516a0-d77e-11de-89a3-1386611daa76>
2009-12-04 14:35:50 +0000

    Large update to the Focus.

--- Java/src/controller/FocusFactory.java
+++ Java/src/controller/FocusFactory.java
@@ -1,8 +1,29 @@
 package controller;
 
+import model.focus.DeadlineFocus;
+import model.focus.DurationFocus;
+import model.focus.FocusStrategy;
+import model.focus.FocusWork;
+
 public class FocusFactory {
 	FocusFactory()
 	{
 		
+	}
+	 public enum FocusType{
+		 DurationFocus,
+		 DeadlineFocus
+	 }
+	public static FocusWork createFocus(FocusType type)
+	{
+		switch(type)
+		{
+		case DurationFocus:
+			return new FocusWork(new DurationFocus(0, 0));
+		case DeadlineFocus:
+			return new FocusWork(new DeadlineFocus(0));
+		default:
+			return new FocusWork(new FocusStrategy());
+		}
 	}
 }
--- Java/src/model/focus/DeadlineFocus.java
+++ Java/src/model/focus/DeadlineFocus.java
@@ -12,10 +12,18 @@ public class DeadlineFocus extends FocusStrategy {
 	public DeadlineFocus(int amount) {
 		this.amount = amount;
 	}
-	@Override
-	public List<Task> showtasks() {
-		List<Task> allTasks = this.getCurrentUser().getTasks();
-		Collections.sort(allTasks,new TaskDeadlineComparator());
-		return allTasks.subList(0, amount - 1);
+	protected List<Task> sort(List<Task> tasks)
+	{
+		Collections.sort(tasks,new TaskDeadlineComparator());
+		return tasks;
+	}
+	/**
+	 * Method that returns only a few tasks out of a list. This 'few' is defined by the given amount
+	 * @param tasks Original list of tasks
+	 * @return sublist of the original list
+	 */
+	protected List<Task> filter(List<Task> tasks)
+	{
+		return tasks.subList(0, amount - 1);
 	}
 }
