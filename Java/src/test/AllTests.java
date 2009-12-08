3e707e86b7b36904165df6e4b84afa24ba06b5fa
vangeneugden <vangeneugden@8ba516a0-d77e-11de-89a3-1386611daa76>
2009-12-08 22:40:44 +0000

    Bestaande tests juist gezet Begonnen aan tests voor Focus

--- Java/src/test/AllTests.java
+++ Java/src/test/AllTests.java
@@ -1,8 +1,16 @@
 package test;
 
+import model.ResourceTest;
+import model.TaskTest;
+import model.UserTest;
+
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
+
+import controller.ProjectControllerTest;
+import controller.ResourceControllerTest;
+import controller.TaskControllerTest;
 
 @RunWith(value=Suite.class)
 @SuiteClasses(value={UserTest.class,TaskTest.class,ResourceTest.class,ProjectControllerTest.class,ResourceControllerTest.class,TaskControllerTest.class})
