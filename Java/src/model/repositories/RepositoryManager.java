fffcb1d4ae9a279bd063720e749d18bcbf39b867
vangeneugden <vangeneugden@8ba516a0-d77e-11de-89a3-1386611daa76>
2009-12-10 15:04:28 +0000

    Aanmaken van repository klassen. Vanaf volgende commits begint de doorvoering van de wijzigingen in de rest van de code

---
+++ Java/src/model/repositories/RepositoryManager.java
@@ -0,0 +1,55 @@
+package model.repositories;
+
+import java.util.List;
+
+import model.Project;
+import model.Resource;
+import model.User;
+
+public class RepositoryManager {
+    private Repository<Project> projectRepository;
+    private Repository<User> userRepository;
+    private Repository<Resource> resourceRepository;
+    
+    public RepositoryManager()
+    {
+        projectRepository = new Repository<Project>();
+        userRepository = new Repository<User>();
+        resourceRepository = new Repository<Resource>();
+    }
+    
+    public boolean add(Project p)
+    {
+        return projectRepository.add(p);
+    }
+    
+    public boolean add(Resource r)
+    {
+        return resourceRepository.add(r);
+    }
+    
+    public boolean remove(Project p)
+    {
+        return projectRepository.remove(p);
+    }
+    
+    public boolean remove(Resource r)
+    {
+        return resourceRepository.remove(r);
+    }
+    
+    public List<Resource> getResources()
+    {
+        return resourceRepository.getAll();
+    }
+    
+    public List<Project> getProjects()
+    {
+        return projectRepository.getAll();
+    }
+    
+    public List<User> getUsers()
+    {
+        return userRepository.getAll();
+    }
+}
