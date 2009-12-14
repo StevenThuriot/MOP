3066241f2cdc73181d86f161a06bd152be65fd32
Steven Thuriot <StevenThuriot@users.noreply.github.com>
2009-12-14 12:34:18 +0000

    Corrected BR3 in the Successful state

--- Java/src/model/SuccessfulTaskState.java
+++ Java/src/model/SuccessfulTaskState.java
@@ -93,22 +93,13 @@ public class SuccessfulTaskState extends TaskState {
 	{
 		GregorianCalendar currentTime = new GregorianCalendar();
 		GregorianCalendar startTime = this.getContext().getStartDate();
-		GregorianCalendar dueTime = this.getContext().getDueDate();
-		boolean answer = false;
 		
 		//Not before start time
 		if ( !currentTime.before(startTime) ) {
-			//Rule succeeds, continue to next check
-			answer = true;
+			return true;
 		}
 		
-		//Not after or at the duetime
-		if ( !( answer && !currentTime.before(dueTime) )) {
-			//Rule fails
-			answer = false;
-		}
-		
-		return answer;
+		return false;
 	}
 
 	@Override
