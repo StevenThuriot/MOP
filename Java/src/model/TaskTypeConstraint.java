a04708da4636939aca08b885c98ca389c43e13c4
kwintenmissiaen@gmail.com <kwintenmissiaen@gmail.com@8ba516a0-d77e-11de-89a3-1386611daa76>
2010-05-15 11:15:43 +0000

    Terug één type TaskTypeConstraint. Bevat voorlopig geen methode 'satisfied()', maar wel het benodigde AssetType en de benodigde hoeveelheid. Heb ook TaskType aangepast.
    Sorry voor geen unit tests - ik weet niet goed hoe ik deze moet schrijven wanneer er nog geen constructoren voor Assets en AssetTypes voorhanden zijn.
    
    Ik begin me trouwens  ook af te vragen wat het nut is van ons design met TaskTypeFactory. Er zit in TaskType momenteel geen enkele referentie naar een specifieke taak, ik denk dan ook niet dat we momenteel een clone aan moeten maken. Momenteel heb ik de indruk dat een gewone referentie naar een bestaand TaskType object moet volstaan. Blijft enkel de taak over om de TaskTypes aan te maken bij het lezen van de XML.

--- Java/src/model/TaskTypeConstraint.java
+++ Java/src/model/TaskTypeConstraint.java
@@ -2,11 +2,41 @@ package model;
 
 public abstract class TaskTypeConstraint {
 	
+	/**
+	 * The required AssetType.
+	 */
+	private AssetType assetT;
 	
 	/**
-	 * Indicates whether this constraint is satisfied or not.
-	 * @return
+	 * The minimum amount of assets of specified type required.
 	 */
-	public abstract boolean satisfied(Task t);
+	private int min;
+	
+	/**
+	 * The maximum amount of assets of specified type required.
+	 */
+	private int max;
+	
+	/**
+	 * Creates a TaskTypeConstraint for a given AssetType <type>
+	 * and a given amount <nb>
+	 */
+	public TaskTypeConstraint(AssetType type, int min, int max){
+		assetT = type;
+		this.min = min;
+		this.max = max;
+	}
+	
+	public AssetType getAssetType(){
+		return assetT;
+	}
+	
+	public int getMinimum(){
+		return min;
+	}
+	
+	public int getMaximum(){
+		return max;
+	}
 	
 }
