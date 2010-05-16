da478753bdd0848b9fd49c2a0386f42f10033831
kwintenmissiaen@gmail.com <kwintenmissiaen@gmail.com@8ba516a0-d77e-11de-89a3-1386611daa76>
2010-05-16 21:19:14 +0000

    attribuut taskT in Task + constructoren aangepast niet aangepast: constructoraanroepen in andere klassen (deze moeten toch vervangen worden door factory methode)... sorry voor nog meer errors dus
    eenvoudige unit tests voor TaskType

---
+++ Java/test/model/TaskTypeTest.java
@@ -0,0 +1,30 @@
+package model;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TaskTypeTest {
+
+	UserType uT;
+	Asset asset;
+	TaskType taskT;
+	
+	@Before
+	public void setUp(){
+		taskT = new TaskType("Playing Go", new ArrayList<Field>()
+				, new ArrayList<TaskTypeConstraint>());
+	}
+	
+	@Test
+	public void init(){
+		assertTrue(taskT.getTemplate().isEmpty());
+		assertTrue(taskT.getConstraints().isEmpty());
+		assertTrue(taskT.getName().equals("Playing Go"));
+	}
+	
+	
+}
