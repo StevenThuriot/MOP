7bbd392c4950d966dd6e9fbb78806b871a0473a9
Steven Thuriot <StevenThuriot@users.noreply.github.com>
2010-05-18 10:03:07 +0000

    - setValue() in field gefixed. - Extra test om dit aan te tonen. - Comments in de field classes bijgevoegd.

--- Java/src/model/Field.java
+++ Java/src/model/Field.java
@@ -1,33 +1,66 @@
 package model;
 
 abstract class Field<T> {
+	/**
+	 * Field name
+	 */
 	private String name;
+	/**
+	 * Type of field
+	 */
 	private FieldType type;
-	private T value;
+	/**
+	 * The value of the field
+	 */
+	protected T value;
 	
+	/**
+	 * Constructor
+	 * @param name Field name
+	 * @param type Type of field
+	 * @param value The value of the field
+	 */
 	public Field(String name, FieldType type, T value) {
 		this.setName(name);
 		this.type = type;
 		this.setValue(value);
 	}
 	
+	/**
+	 * Get the name of the field
+	 * @return Field name
+	 */
 	public final String getName() {
 		return name;
 	}
 	
+	/**
+	 * Set the name of the field
+	 * @param name The new name
+	 */
 	public final void setName(String name) {
 		this.name = name;
 	}
 	
+	/**
+	 * Get the type of the field
+	 * @return Enumeration of the type.
+	 */
 	public final FieldType getType() {
 		return type;
 	}
 	
+	/**
+	 * Get the current value of the field.
+	 * @return Current value
+	 */
 	public final T getValue() {
 		return value;
 	}
 
-	public final void setValue(T value) {
-		this.value = value;
-	}
+	/**
+	 * Set the current value of the field
+	 * @param value The new value
+	 */
+	public abstract void setValue(T value);
 }
--- Java/src/model/NumericField.java
+++ Java/src/model/NumericField.java
@@ -1,9 +1,21 @@
 package model;
 
 public class NumericField extends Field<Integer> {
+	/**
+	 * Field constructor for integers.
+	 * @param name Field name
+	 * @param value Field value
+	 */
 	public NumericField(String name, int value) {
 		super(name, FieldType.Numeric, value);
 	}
-	
-	
+
+	/**
+	 * Set the current value of the field
+	 * @param value The new value
+	 */
+	@Override
+	public void setValue(Integer value) {
+		this.value = value;
+	}	
 }
