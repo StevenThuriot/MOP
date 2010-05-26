f1071a942f86b9d5bae0a687e43c89d4278c9183
steventhuriot <steventhuriot@8ba516a0-d77e-11de-89a3-1386611daa76>
2010-05-26 16:45:47 +0000

    Gbye parser tests errors

--- Java/test/controller/XMLControllerTest.java
+++ Java/test/controller/XMLControllerTest.java
@@ -1,16 +1,18 @@
 package controller;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
 import javax.naming.NameNotFoundException;
 
-import model.Resource;
+import model.User;
 import model.repositories.RepositoryManager;
 import org.junit.After;
 import org.junit.Before;
@@ -38,114 +40,71 @@ import exception.WrongFieldsForChosenTypeException;
 import exception.WrongUserForTaskTypeException;
 
 public class XMLControllerTest {
-    private XMLController controller;
-    private RepositoryManager manager;
-    private DispatchController dcontroller;
-    @Before
-    public void setUp() throws TimeException, ParseException
-    {
-        manager = new RepositoryManager();
-        dcontroller = new DispatchController(manager);
-        controller = dcontroller.getXmlController();
-        
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss", Locale.US);
-        Date date = sdf.parse("2009-10-20T20:00:00");
-	    GregorianCalendar gregDate = new GregorianCalendar();
-	    gregDate.setTime(date);
+	 	private XMLController parser;
+	    private RepositoryManager manager;
+	    private DispatchController dcontroller;    
+	    Map<String, User> users = new HashMap<String, User>();
 	    
-        manager.getClock().setTime(gregDate);
-    }
-    @After
-    public void tearDown()
-    {
-        manager = null;
-        controller  = null;
-        dcontroller = null;
-    }
-    
-    /**
-     * Hardcoded tests the parser for the given model in students_public.xml
-     * This test simply tests amounts of objects in their Information Experts
-     * @throws NameNotFoundException
-     * @throws DOMException
-     * @throws EmptyStringException
-     * @throws ParseException
-     * @throws BusinessRule1Exception
-     * @throws DependencyCycleException
-     * @throws IllegalStateCallException 
-     * @throws NullPointerException 
-     * @throws BusinessRule3Exception 
-     * @throws NotAvailableException 
-     * @throws UnknownStateException 
-     * @throws BusinessRule2Exception 
-     * @throws IllegalStateChangeException 
-     * @throws AssetConstraintFullException 
-     * @throws AssetTypeNotRequiredException 
-     * @throws WrongUserForTaskTypeException 
-     * @throws NonExistingTypeSelected 
-     * @throws WrongFieldsForChosenTypeException 
-     * @throws AssetAllocatedException 
-     * @throws NoReservationOverlapException 
-     * @throws TimeException 
-     * @throws InvitationInvitesOwnerException 
-     * @throws InvitationNotPendingException 
-     */
-    @Test
-    public void testModelParseAmounts() throws NameNotFoundException, DOMException, EmptyStringException, ParseException, BusinessRule1Exception, DependencyCycleException, NullPointerException, IllegalStateCallException, BusinessRule3Exception, NotAvailableException, UnknownStateException, IllegalStateChangeException, BusinessRule2Exception, NoReservationOverlapException, AssetAllocatedException, WrongFieldsForChosenTypeException, NonExistingTypeSelected, WrongUserForTaskTypeException, AssetTypeNotRequiredException, AssetConstraintFullException, TimeException, InvitationInvitesOwnerException, InvitationNotPendingException
-    {
-        controller.parse("students_public.xml","theme_development_1.xml", dcontroller);
-        
-        assertEquals(2,manager.getProjects().size());
-        assertEquals(4,manager.getResources().size());
-
-        
-        
-        //assertEquals(4,result.getTasks().size());
-        //assertEquals(4, dcontroller.getResourceController().getReservations().size());
-    }
-    
-    /**
-     * This test will test the relations between different objects
-     * @throws NameNotFoundException
-     * @throws DOMException
-     * @throws EmptyStringException
-     * @throws ParseException
-     * @throws BusinessRule1Exception
-     * @throws DependencyCycleException
-     * @throws IllegalStateCallException 
-     * @throws NullPointerException 
-     * @throws BusinessRule3Exception 
-     * @throws NotAvailableException 
-     * @throws UnknownStateException 
-     * @throws BusinessRule2Exception 
-     * @throws IllegalStateChangeException 
-     * @throws AssetConstraintFullException 
-     * @throws AssetTypeNotRequiredException 
-     * @throws WrongUserForTaskTypeException 
-     * @throws NonExistingTypeSelected 
-     * @throws WrongFieldsForChosenTypeException 
-     * @throws AssetAllocatedException 
-     * @throws NoReservationOverlapException 
-     * @throws TimeException 
-     * @throws InvitationInvitesOwnerException 
-     * @throws InvitationNotPendingException 
-     */
-    @Test
-    public void testRelations() throws NameNotFoundException, DOMException, EmptyStringException, ParseException, BusinessRule1Exception, DependencyCycleException, NullPointerException, IllegalStateCallException, BusinessRule3Exception, NotAvailableException, UnknownStateException, IllegalStateChangeException, BusinessRule2Exception, NoReservationOverlapException, AssetAllocatedException, WrongFieldsForChosenTypeException, NonExistingTypeSelected, WrongUserForTaskTypeException, AssetTypeNotRequiredException, AssetConstraintFullException, TimeException, InvitationInvitesOwnerException, InvitationNotPendingException
-    {
-        controller.parse("students_public.xml","theme_development_1.xml", dcontroller);
-        Resource devRoom = manager.getResources().get(0); //Should be the 'Development room' resource
-
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss", Locale.US);
-        Date date = sdf.parse("2009-10-01T08:00:00");
-	    GregorianCalendar gregDate = new GregorianCalendar();
-	    gregDate.setTime(date);
-        
-        assertFalse(devRoom.availableAt(gregDate, 10)); //Should comply with the reservation at 2009-10-21T08:00:00 for 3060 minutes
-        
-        //Task taskMakeDesign = result.getTasks().get(2); //Should be the task 'Make UML Design'
-        //assertTrue(taskMakeDesign.getRequiredResources().contains(devRoom)); //This task requires the dev room
-    }
-    
-    
+	    
+	    @Before
+	    public void setUp() throws TimeException, ParseException, NameNotFoundException, DOMException, NullPointerException, EmptyStringException, BusinessRule1Exception, DependencyCycleException, IllegalStateCallException, BusinessRule3Exception, NotAvailableException, UnknownStateException, IllegalStateChangeException, BusinessRule2Exception, NoReservationOverlapException, AssetAllocatedException, WrongFieldsForChosenTypeException, NonExistingTypeSelected, WrongUserForTaskTypeException, AssetTypeNotRequiredException, AssetConstraintFullException, InvitationInvitesOwnerException, InvitationNotPendingException
+	    {
+	    	manager = new RepositoryManager();
+	        dcontroller = new DispatchController(manager);
+	        parser = dcontroller.getXmlController();
+	        
+	        ArrayList<User> parsedUsers = parser.parse("students_public.xml", "theme_development_1.xml", dcontroller);
+			
+			for (User user : parsedUsers) {
+				manager.add(user);
+				users.put(user.getName(), user);
+			}
+	    }
+	    
+	    @After
+	    public void tearDown()
+	    {
+	        manager = null;
+	        parser  = null;
+	        dcontroller = null;
+	        users = null;
+	    }
+	    /**
+	     * Testing system time
+	     * @throws ParseException
+	    */
+	    @Test
+	    public void timeTest() throws ParseException
+	    {
+	    	SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss", Locale.US);
+	        Date date = sdf.parse("2009-10-22T00:00:00");
+		    GregorianCalendar gregDate = new GregorianCalendar();
+		    gregDate.setTime(date);
+		    
+	    	assertEquals(gregDate, manager.getClock().getTime());
+	    }
+	    
+	    /**
+	     * Hardcoded tests the parser for the given model in students_public.xml
+	     * This test simply tests amounts of objects in their Information Experts
+	     * @throws DOMException
+	     * @throws NullPointerException 
+	     */
+	    @Test
+	    public void testModelParseAmounts() throws DOMException, NullPointerException
+	    {
+	    	assertEquals(3, users.size());
+	        assertEquals(1, manager.getProjects().size());
+	        assertEquals(6, manager.getResources().size());
+	        
+	        User Bob = users.get("Bob");
+	        User Eve = users.get("Eve");
+	        User Alice = users.get("Alice");
+	        
+	        assertEquals(2, Bob.getAllInvitations().size());
+	        
+	        assertEquals(0, Bob.getTasks().size());
+	        assertEquals(0, Eve.getTasks().size());
+	        assertEquals(5, Alice.getTasks().size());
+	    }    
 }
